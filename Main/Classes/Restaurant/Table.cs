3a498d45659c50053cbac37bff5c2eae40a25050
Ugur Can Saltik <cansaltik1903@gmail.com>
2025-11-11 16:01:04 +0100

    Table.cs class added

---
+++ Main/Classes/Restaurant/Table.cs
@@ -0,0 +1,39 @@
+﻿namespace Main.Classes.Restaurant;
+
+[Serializable]
+public class Table
+{
+    public int TableId { get; }
+    private int _number;
+    public int Number
+    {
+        get => _number;
+        set
+        {
+            if (value <= 0)
+                throw new ArgumentException("Table number can't be zero or negative");
+            _number = value;
+        }
+    }
+    private static List<Table> _extent = new List<Table>();
+    public Table(int tableId, int number)
+    {
+        if(tableId <= 0)
+            throw new ArgumentException("Table id can't be zero or negative");
+        TableId = tableId;
+        Number=number;
+        AddToExtent(this);
+    }
+
+    private static void AddToExtent(Table table)
+    {
+        if(table==null)
+            throw new ArgumentNullException(nameof(table));
+        _extent.Add(table);
+    }
+
+    public static IReadOnlyList<Table> GetExtent() 
+        => _extent.AsReadOnly();
+    public static void ClearExtentForTests()
+        => _extent.Clear();
+}
