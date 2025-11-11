7c8612a763e956691c21cf3f5fffdfa56167b10c
Ugur Can Saltik <cansaltik1903@gmail.com>
2025-11-11 23:28:55 +0100

    FullTime and PartTime roles for staff added

---
+++ Main/Classes/Employees/FullTime.cs
@@ -0,0 +1,33 @@
+﻿namespace Main.Classes.Employees;
+
+public enum Shift
+{
+    Morning, Evening, Night
+}
+
+[Serializable]
+public class FullTime : Staff
+{
+    public Shift Shift { get; private set; }
+
+    public FullTime(string firstName, string lastName, decimal salary, string department, Shift shift)
+        : base(firstName, lastName, salary, department)
+    {
+        Shift = shift;
+    }
+
+    public void ShiftChange(Shift newShift)
+    {
+        Shift = newShift;
+    }
+
+    public override void hireStaff()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void fireStaff()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Main/Classes/Employees/PartTime.cs
@@ -0,0 +1,49 @@
+﻿namespace Main.Classes.Employees;
+
+[Serializable]
+public class PartTime : Staff
+{
+    private int _hours;
+    private decimal _hourlyRate;
+
+    public int Hours
+    {
+        get => _hours;
+        set
+        {
+            if (value <= 0) 
+                throw new ArgumentException("Hours can't be zero or negative");
+            _hours = value;
+            Salary = WeeklySalary;
+        }
+    }
+    public decimal HourlyRate
+    {
+        get => _hourlyRate;
+        set
+        {
+            if (value <= 0) 
+                throw new ArgumentException("HourlyRate can't be zero or negative");
+            _hourlyRate = value;
+            Salary = WeeklySalary;
+        }
+    }
+    public decimal WeeklySalary => Hours * HourlyRate;
+
+    public PartTime(string firstName, string lastName, string department, int hours, decimal hourlyRate)
+        : base(firstName, lastName,0m ,department)
+    {
+        Hours = hours;
+        HourlyRate = hourlyRate;
+        Salary = WeeklySalary;
+    }
+    public override void hireStaff()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void fireStaff()
+    {
+        throw new NotImplementedException();
+    }
+}
