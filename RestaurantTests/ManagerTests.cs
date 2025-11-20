5123097ef56758047113524ae0a1058540d39a85
Ugur Can Saltik <cansaltik1903@gmail.com>
2025-11-20 20:42:51 +0100

    Tests organized accordingly and main test class was renamed. Added more tests for Part and Full time.

---
+++ RestaurantTests/ManagerTests.cs
@@ -0,0 +1,98 @@
+﻿using Main.Classes.Employees;
+
+namespace RestaurantTests;
+
+[TestFixture]
+public class ManagerTests
+{
+    private string _testFilePath;
+    
+    [SetUp]
+    public void Setup()
+    {
+        var extentField = typeof(Manager).GetField("_managerExtent");
+        extentField?.SetValue(null, new List<Manager>());
+        
+        _testFilePath = Path.Combine(Path.GetTempPath(), $"managers_test_{Guid.NewGuid()}.json");
+    }
+    
+    [TearDown]
+    public void Teardown()
+    {
+        if (File.Exists(_testFilePath))
+            File.Delete(_testFilePath);
+    }
+    
+    [Test]
+    public void SaveAndLoad()
+    {
+        new Manager("Arda", "Seydol", 7000m, "Management", ManagerLevels.Senior);
+        
+        Manager.Save(_testFilePath);
+        
+        var extentField = typeof(Manager).GetField("_managerExtent");
+        extentField?.SetValue(null, new List<Manager>());
+        
+        bool result = Manager.Load(_testFilePath);
+        var extent = Manager.GetExtent();
+        
+        Assert.That(result, Is.True);
+        Assert.That(extent.Count, Is.EqualTo(1));
+        Assert.That(extent[0].Level, Is.EqualTo(ManagerLevels.Senior));
+    }
+    
+
+    [Test]
+    public void HireStaff()
+    {
+        var manager = new Manager("Derya", "Ogus", 6000m, "Management", ManagerLevels.Mid);
+        
+        using var consoleOutput = new StringWriter();
+        Console.SetOut(consoleOutput);
+        
+        manager.hireStaff();
+        
+        Assert.That(consoleOutput.ToString(), Does.Contain("Manager Ogus is hiring staff."));
+    }
+
+    [Test]
+    public void FireStaff()
+    {
+        var manager = new Manager("Derya", "Ogus", 6000m, "Management", ManagerLevels.Junior);
+        
+        using var consoleOutput = new StringWriter();
+        Console.SetOut(consoleOutput);
+        
+        manager.fireStaff();
+        
+        Assert.That(consoleOutput.ToString(), Does.Contain("Manager Ogus is firing staff."));
+    }
+
+    [Test]
+    public void ManageEmployee()
+    {
+        var manager = new Manager("Derya", "Ogus", 6000m, "Management", ManagerLevels.Senior);
+        
+        using var consoleOutput = new StringWriter();
+        Console.SetOut(consoleOutput);
+        
+        manager.ManageEmployee();
+        
+        Assert.That(consoleOutput.ToString(), Does.Contain("Manager Ogus is managing employees."));
+    }
+
+    [Test]
+    public void ChangeStaffShift()
+    {
+        var manager = new Manager("Derya", "Ogus", 6000m, "Management", ManagerLevels.Mid);
+        
+        using var consoleOutput = new StringWriter();
+        Console.SetOut(consoleOutput);
+        
+        manager.ChangeStaffShift();
+        
+        Assert.That(consoleOutput.ToString(), Does.Contain("Manager Ogus is changing staff shift."));
+    }
+
+   
+}
